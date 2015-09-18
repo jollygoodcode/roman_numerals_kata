931abc921b83d563856dfd81ebd305fc418e1852
Juanito Fatas <katehuang0320@gmail.com>
2015-09-18 20:21:09 +0800

    Make it pass

--- converter.rb
+++ converter.rb
@@ -2,6 +2,11 @@ class Converter
   def decimal_to_roman(decimal)
     n, result = decimal, ""
 
+    if n >= 10
+      result << "X"
+      n -= 10
+    end
+
     if n >= 5
       result << "V"
       n -= 5
