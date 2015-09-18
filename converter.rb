bac87e62cfa4a12449e7415688800a7a49294172
Juanito Fatas <katehuang0320@gmail.com>
2015-09-18 20:33:58 +0800

    Make it pass

--- converter.rb
+++ converter.rb
@@ -1,11 +1,14 @@
 class Converter
   DECIMAL_ROMAN = [
-    [10, "X"],
+    [90, "XC"],
+    [50,  "L"],
+    [40, "XL"],
+    [10,  "X"],
 
-    [9, "IX"],
-    [5,  "V"],
-    [4, "IV"],
-    [1,  "I"],
+    [9,  "IX"],
+    [5,   "V"],
+    [4,  "IV"],
+    [1,   "I"],
   ]
 
   def decimal_to_roman(decimal)
