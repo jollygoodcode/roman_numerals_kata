71de50eaa7e1ff227e72aaa177edd295274eb2a1
Juanito Fatas <katehuang0320@gmail.com>
2015-09-18 20:29:33 +0800

    Don't Repeat Yourself

--- converter.rb
+++ converter.rb
@@ -1,25 +1,19 @@
 class Converter
+  DECIMAL_ROMAN = [
+    [10, "X"],
+    [5,  "V"],
+    [4, "IV"],
+    [1,  "I"],
+  ]
+
   def decimal_to_roman(decimal)
     n, result = decimal, ""
 
-    while n >= 10
-      result << "X"
-      n -= 10
-    end
-
-    while n >= 5
-      result << "V"
-      n -= 5
-    end
-
-    while n >= 4
-      result << "IV"
-      n -= 4
-    end
-
-    while n >= 1
-      result << "I"
-      n -= 1
+    DECIMAL_ROMAN.each do |value, roman|
+      while n >= value
+        result << roman
+        n -= value
+      end
     end
 
     result
