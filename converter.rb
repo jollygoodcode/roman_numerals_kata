927d52ae9703ec99b421089416518c0e7978c1b6
Juanito Fatas <katehuang0320@gmail.com>
2015-09-20 00:41:24 +0800

    Make it pass

--- converter.rb
+++ converter.rb
@@ -3,7 +3,23 @@ class Converter
     raise "NotImplementedError"
   end
 
+  ROMAN_VALUES = {
+    "M"  => 1000,
+    "CM" => 900,
+    "D"  => 500,
+    "CD" => 400,
+    "C"  => 100,
+    "XC" => 90,
+    "L"  => 50,
+    "XL" => 40,
+    "X"  => 10,
+    "IX" => 9,
+    "V"  => 5,
+    "IV" => 4,
+    "I"  => 1
+  }
+
   def roman_to_decimal(roman)
-    raise "NotImplementedError"
+    ROMAN_VALUES.fetch(roman) { 0 }
   end
 end
